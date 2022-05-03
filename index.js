3ba598bf4c8e3489dbb1f56344d650e9f09fa0de
puckfried <puckfried@posteo.de>
2022-05-03 12:04:31 +0200

    Zahlen komplett

--- index.js
+++ index.js
@@ -52,3 +52,53 @@ console.log(98e6);
 console.log(98e-6);
 
 
+// Math Object
+
+// Was ist ein Objekt:
+// let meinObjekt = {
+//     name: "Christoph",
+//     age: 38
+// }
+
+// console.log(meinObjekt.name);
+
+// Math ist ein vordefiniertes Objekt
+// Hat Konstante werte wie:
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+let ganzzahl = 2;
+let float   = 2.54; 
+
+// vordefinierte Methoden
+
+// Runden
+console.log(Math.round(float)); // rundet nach Rundungsregeln
+console.log(Math.floor(float)); // rundet nach unten
+console.log(Math.ceil(float)); // rundet nach oben
+
+// min und max
+console.log(Math.max(1,2,30,45,5)); //sucht die größte Zahl aus einer Zahlenreihe
+console.log(Math.min(23,45,2,3456)); //sucht die kleinste Zahl
+
+// random erzeugt eine Zufallszahl zwischen 0 und 1
+console.log("Zufallszahl:" ,Math.floor(Math.random()*10)); // erzeugt eine Zufallszahl zwischen 0-9
+
+
+// Zahlenraten
+let meineZahl = 2
+let random = Math.floor(Math.random()*10);
+meineZahl === random ? console.log('Gewonnen') : console.log('Verloren!');
+
+
+// Farbgenerator rgb
+let beispielfarbe = "rgb(255,255,255)";
+
+let rot = Math.floor(Math.random()*256);
+let gruen = Math.floor(Math.random()*256);
+let blau = Math.floor(Math.random()*256);
+
+let unsereFarbe = `rgb(${rot}, ${gruen}, ${blau})`;
+console.log(unsereFarbe);
+
+
